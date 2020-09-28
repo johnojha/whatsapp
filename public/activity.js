20504054f178f68f236a5c4cc7f9372b3496fc02
johnojha <john_ojha@yahoo.com>
2020-09-28 13:06:15 +0530

    update activity.js

--- public/activity.js
+++ public/activity.js
@@ -4,7 +4,7 @@ var connection = new Postmonger.Session();
 connection.trigger('ready');
 
 connection.on('initActivity', function( data ) {
-  document.getElementById( 'configuration' ).value = JSON.stringify( data, null, 2 );
+  //document.getElementById( 'configuration' ).value = JSON.stringify( data, null, 2 );
 });
 
 // Save Sequence
